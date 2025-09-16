3e3684bebcf1408401578763578c52e01b0231ec
McKinnon Porter <mbporter@email.sc.edu>
2025-09-16 19:25:08 -0400

    adding images for flipbook and old and new laptop

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const images = [
-    'images/image1.jpg', 'images/image2.jpg',
-    'images/image3.jpg', 'images/image4.jpg',
-    'images/image5.jpg', 'images/image6.jpg',
-    'images/image7.jpg', 'images/image8.jpg',
-    'images/image9.jpg', 'images/image10.jpg',
-    'images/image11.jpg', 'images/image12.jpg',
-    'images/image13.jpg', 'images/image14.jpg',
-    'images/image15.jpg', 'images/image16.jpg',
-    'images/image17.jpg', 'images/image18.jpg',
-    'images/image19.jpg', 'images/image20.jpg',
-    'images/image21.jpg', 'images/image22.jpg',
-    'images/image23.jpg', 'images/image24.jpg',
-    'images/image25.jpg', 'images/image26.jpg',
-    'images/image27.jpg', 'images/image28.jpg'
+    'images/image1.jpeg', 'images/image2.jpeg',
+    'images/image3.jpeg', 'images/image4.jpeg',
+    'images/image5.jpeg', 'images/image6.jpeg',
+    'images/image7.jpeg', 'images/image8.jpeg',
+    'images/image9.jpeg', 'images/image10.jpeg',
+    'images/image11.jpeg', 'images/image12.jpeg',
+    'images/image13.jpeg', 'images/image14.jpeg',
+    'images/image15.jpeg', 'images/image16.jpeg',
+    'images/image17.jpeg', 'images/image18.jpeg',
+    'images/image19.jpeg', 'images/image20.jpeg',
+    'images/image21.jpeg', 'images/image22.jpeg',
+    'images/image23.jpeg', 'images/image24.jpeg',
+    'images/image25.jpeg', 'images/image26.jpeg',
+    'images/image27.jpeg', 'images/image28.jpeg'
 ];
-
+ 
 document.addEventListener('DOMContentLoaded', function() {
     let currentPage = 0;
     const totalSpreads = images.length / 2; // 14 spreads
